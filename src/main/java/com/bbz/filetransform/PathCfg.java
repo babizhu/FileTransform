94097c5a85b7737bd289d0edb70864ed8f8d8c15
liukun <liu_kun@msn.com>
2014-12-26 14:37:40 +0800

    解决了propties文件的中文字符集问题，确保该文件采用utf-8的方式保存

--- src/main/java/com/bbz/filetransform/PathCfg.java
+++ src/main/java/com/bbz/filetransform/PathCfg.java
@@ -1,17 +1,16 @@
 package com.bbz.filetransform;
 
-import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 /**
  * user         LIUKUN
  * time         2014-4-7 13:38
- *
+ * <p/>
  * 服务器的整体配置文件
- *
  */
 
 public class PathCfg{
@@ -19,44 +18,50 @@ public class PathCfg{
     /**
      * EXCEL文件所在的路径
      */
-    public static final String          EXCEL_PATH;
+    public static final String EXCEL_PATH;
 
     /**
      * EXCEL对应的xml输出目录
      */
-    public static final String          EXCEL_OUTPUT_XML_PATH;
+    public static final String EXCEL_OUTPUT_XML_PATH;
 
     /**
      * EXCEL对应的json输出目录
      */
-    public static final String          EXCEL_OUTPUT_JSON_PATH;
+    public static final String EXCEL_OUTPUT_JSON_PATH;
 
 
     /**
      * EXCEL对应的java读取excel的输出目录
      */
-    public static final String          EXCEL_OUTPUT_JAVA_PATH;
+    public static final String EXCEL_OUTPUT_JAVA_PATH;
 
 
     /**
      * 数据库的模板文件路径
      */
-    public static final String          DB_TEMPLET_PATH;
-
+    public static final String DB_TEMPLET_PATH;
 
 
     /**
      * java代码的模板文件路径
      */
-    public static final String          JAVA_TEMPLET_PATH;
+    public static final String JAVA_TEMPLET_PATH;
 
 
-
-    static {
+    static{
         Properties prop = new Properties();
         try {
-            InputStream in = new BufferedInputStream( new FileInputStream( "resource/path.properties" ) );
-            prop.load( in );
+//            InputStream in = new BufferedInputStream( new FileInputStream( "resource/path.properties" ) );
+//            prop.load( in );
+//            in.close();
+            File file = new File( "resource/path.properties" );
+            FileInputStream inputStream = new FileInputStream( file );
+            InputStreamReader streamReader = new InputStreamReader( inputStream );
+            prop.load( streamReader );
+
+            inputStream.close();
+            streamReader.close();
 
         } catch( IOException e ) {
             e.printStackTrace();
@@ -72,7 +77,7 @@ public class PathCfg{
     }
 
     public static void main( String[] args ){
-        System.out.println( PathCfg.EXCEL_OUTPUT_JAVA_PATH );
+        System.out.println( PathCfg.EXCEL_PATH );
     }
 }
 
